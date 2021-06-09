1b8bfb570397c51b68b3288f636e1baea7b74234
Alexis <alexis9653@hotmail.fr>
2021-06-09 10:01:38 +0200

    -- commentaires

--- Challenge_explorateur_3.php
+++ Challenge_explorateur_3.php
@@ -19,24 +19,33 @@ $populations = $data['populations'];
 $populations_dechiffrable = '';
 
 foreach ($populations as $population) {
+    // Je sépare en deux parties chaque éléments du tableau
     $code_population = explode(':', $population)[0];
     $extrait_du_langage = explode(':', $population)[1];
 
+    // Je fais appel à une fonction qui fait toute les vérifications
+    // Si l'extrait passe tout les tests j'ajoute le code de la population à ma chaîne de caractères
     if (processAllChecks($extrait_du_langage)) {
         $populations_dechiffrable .= $code_population;
     }
 }
 
+// Si aucune des population n'a de langage déchiffrable
 if ($populations_dechiffrable == '') {
     $populations_dechiffrable == 'NOPOPULATION';
 }
 
-print($populations_dechiffrable);
-
-function processAllChecks($extrait_du_langage)
+/**
+ * Cette fonction procède à une batterie de test sur l'extrait de langage
+ * Si tout les tests sont réussi elle retourne true, false sinon
+ * @param string $extrait_du_langage
+ * @return boolean
+ */
+function processAllChecks(string $extrait_du_langage): bool
 {
     $flag = false;
 
+    // Certains test on besoin d'un tableau qui liste tout les mots de l'extrait
     $wordsList = explode(' ', $extrait_du_langage);
 
     if (
@@ -50,9 +59,23 @@ function processAllChecks($extrait_du_langage)
     return $flag;
 }
 
-function checkNumberOfUniqueChar($extrait_du_langage)
+/**
+ * Cette fonction compte le nombre de caractères unique dans l'extrait
+ * Si il y en a 20 ou plus est retourne true, false sinon  
+ * @param string $extrait_du_langage
+ * @return boolean
+ */
+function checkNumberOfUniqueChar(string $extrait_du_langage): bool
 {
+    // Pour la suite on a besoins de supprimer tout les espaces
     $extrait_sans_espaces = str_replace(' ', '', $extrait_du_langage);
+
+    // Cette ligne fais 3 choses :
+        // - On transforme la chaîne de caractères en tableau
+        // - On transforme ce tableau en tableau associatif ['élement1' => 2, 'élement2' => 1, ...]
+        //   qui compte le nombre d'élément identiques dans un tableau
+        // - On compte les éléments du tableau associciatif
+    // On à donc le nombre de caractères unique
     $uniqueChars = count(array_count_values(str_split($extrait_sans_espaces)));
 
     if ($uniqueChars >= 20) {
@@ -61,15 +84,27 @@ function checkNumberOfUniqueChar($extrait_du_langage)
     return false;
 }
 
-function checkMinimumNumberOfWord($wordsList)
+/**
+ * Cette fonction compte le nombre de mots de plus de 2 caractères qui se répètent plus de 2 fois
+ * Si il y en a 5 ou plus est retourne true, false sinon
+ * @param array $wordsList
+ * @return boolean
+ */
+function checkMinimumNumberOfWord(array $wordsList): bool
 {
     $goodWordCount = 0;
 
+    // Cette fonction compte le nombre d'élément identiques dans un tableau
+    // Elle rend un tableau associatif ['élement1' => 2, 'élement2' => 1, ...]
     $tempArray = array_count_values($wordsList);
 
     foreach ($tempArray as $word => $numberOfRepeat) {
+        // Pour chaque item on vérifie deux choses : 
+            // - Fait-il + de 2 caractères ?
+            // ET 
+            // - Est-ce qu'il se répète + de 2 fois ?
         if (strlen($word) > 2 && $numberOfRepeat > 2) {
-            print($word . '->' . $numberOfRepeat . '<br>');
+            // Si c'est le cas on incrémente le compteur de mots valide.
             $goodWordCount++;
         }
     }
@@ -81,7 +116,13 @@ function checkMinimumNumberOfWord($wordsList)
     return false;
 }
 
-function checkMaximumNumberOfWord($wordsList)
+/**
+ * Cette fonction compte le nombre de mots de de 2 caractères
+ * Si il y en a 10 ou moins est retourne true, false sinon
+ * @param array $wordsList
+ * @return boolean
+ */
+function checkMaximumNumberOfWord(array $wordsList): bool
 {
     $WordOfTwoLettersCount = 0;
     foreach ($wordsList as $word) {
